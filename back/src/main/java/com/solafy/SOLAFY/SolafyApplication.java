e03cee68b881b1aee8f4d6511844cfe838db0b28
juheegg <eespacio95@gmail.com>
2021-01-24 13:56:28 +0900

    init: back 개발환경 구축

---
+++ back/src/main/java/com/solafy/SOLAFY/SolafyApplication.java
@@ -0,0 +1,13 @@
+package com.solafy.SOLAFY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolafyApplication.class, args);
+	}
+
+}
