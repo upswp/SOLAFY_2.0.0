571c64363ae0e18e10e733e3f878a87915634fba
Park Sangwoo <tkddnsos@gmail.com>
2021-02-02 00:22:05 +0900

    feat / SOL-124 / 비밀번호 찾기 UI 개발

---
+++ front/src/pages/user/FindPassword.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="q-pa-md" style="height: 250px">
+    <p id="title">비밀번호 찾기</p>
+    <br />
+    <p id="subTitle">
+      개인정보 확인 후 가입시 입력하신 이메일로 확인 메일을 발송해 드립니다.
+    </p>
+    <div class="column items-center" style="height: 700px ">
+      <div class="col-7">
+        <div id="textInput">
+          <q-input
+            outlined
+            label="이름"
+            placeholder="Name"
+            hint="가입시 입력하신 성함을 입력해 주세요"
+          />
+          <br />
+          <br />
+          <q-input
+            outlined
+            label="SSAFY 기수"
+            placeholder="number"
+            hint="가입시 입력하신 기수를 입력해주세요. ex) 4기 -> 4"
+          />
+          <br />
+          <br />
+          <q-input
+            outlined
+            label="name@example.com"
+            placeholder="email"
+            hint="가입시 입력하신 이메일주소를 입력해주세요."
+          />
+        </div>
+      </div>
+      <div class="col-4">
+        <q-btn id="loginbtn" label="인증하기" />
+      </div>
+      <div class="col">
+        <div class="column items-center">
+          <img
+            style="width : 77px; height : 55px;"
+            src="~src/assets/ssafy_logo.png"
+          />
+          <br />
+          <img
+            style="width : 103px; height : 25px;"
+            src="~src/assets/im_enabling_people_logo.png"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+#title {
+  font-style: inherit;
+  padding-top: 100px;
+  text-align: center;
+  font-size: 20px;
+}
+
+#subTitle {
+  text-align: center;
+  font-size: 12px;
+  padding-bottom: 60px;
+}
+#loginbtn {
+  width: 190px;
+  height: 35px;
+  size: "13px";
+  color: white;
+  align-content: center;
+  background-color: #0094ff;
+}
+#textInput {
+  width: 420px;
+  height: 50px;
+  color: #dfdfdf;
+}
+</style>
