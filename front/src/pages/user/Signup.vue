5e36c8207200de6bab596f38d8b6fe4c5f07a717
Park Sangwoo <tkddnsos@gmail.com>
2021-02-06 19:39:48 +0900

    feat / SOL-121 / 회원가입 상단 바 개발

---
+++ front/src/pages/user/Signup.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>
+      <img src="~src/assets/signup.png" style=" width: 100%;" />
+    </div>
+    <div class="column items-center">
+      <div id="textInput">
+        <p>
+          여러분의 행복한 학습을 위한 <br />
+          <strong>SOLAFY</strong> <br />
+          함께 해주셔서 감사합니다.<br />
+        </p>
+      </div>
+
+      <div>
+        <q-btn id="loginbtn" label="Home" />
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+#loginbtn {
+  width: 190px;
+  height: 35px;
+  size: "13px";
+  color: white;
+  align-content: center;
+  background-color: #0094ff;
+  margin-bottom: 50px;
+}
+#textInput {
+  text-align: center;
+  line-height: 50px;
+  color: #000000;
+}
+#point {
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
