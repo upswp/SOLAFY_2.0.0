a77a4e777c6902c721ab34ceb1e228202f77e5bb
Park Sangwoo <tkddnsos@gmail.com>
2021-02-11 04:41:05 +0900

    fix / SOL-201 / index 하단 footer 오류 해결

--- front/src/pages/Index.vue
+++ front/src/pages/Index.vue
@@ -1,64 +1,59 @@
 <template>
-  <body>
-    <div class="q-pa-md" style="height: 250px">
-      <!-- logo -->
-      <div class="fit row justify-center content-center">
-        <img style="width : 80px; height : 80px;" src="~src/assets/logo.png" />
-      </div>
-      <div class="column items-center" style="height: 700px">
-        <div class="col-4">
-          <q-input
-            filled
-            label="SOLAFY ID"
-            style="width : 450px; height : 52px;"
-          />
-          <br />
-          <br />
-          <q-input
-            filled
-            label="PASSWORD"
-            style="width : 450px; height : 52px;"
-          />
-          <!-- padding 값을 주어 변경 -->
-          <div id="subform">
-            <q-checkbox toggle-order="ft" label="자동로그인" color="primary" />
-            <q-btn flat color="black" label="비밀번호 찾기" />
-          </div>
-        </div>
-        <div class="col-4">
-          <q-btn id="loginbtn" label="로그인" />
-          <br />
-          <q-btn id="joinbtn" flat color="primary" label="회원가입" />
-        </div>
-        <div class="col">
-          <div class="column items-center">
-            <img
-              style="width : 77px; height : 55px;"
-              src="~src/assets/ssafy_logo.png"
-            />
-            <br />
-            <img
-              style="width : 103px; height : 25px;"
-              src="~src/assets/im_enabling_people_logo.png"
-            />
-          </div>
-        </div>
-      </div>
-      <Footer></Footer>
+  <div style="height: 250px">
+    <!-- logo -->
+    <div class="fit row justify-center content-center">
+      <img style="width : 80px; height : 80px;" src="~src/assets/logo.png" />
     </div>
-  </body>
+    <div class="column items-center" style="height: 700px">
+      <div class="col-4">
+        <q-input
+          filled
+          label="SOLAFY ID"
+          style="width : 450px; height : 52px;"
+        />
+        <br />
+        <br />
+        <q-input
+          filled
+          label="PASSWORD"
+          style="width : 450px; height : 52px;"
+        />
+        <!-- padding 값을 주어 변경 -->
+        <div id="subform">
+          <q-checkbox toggle-order="ft" label="자동로그인" color="primary" />
+          <q-btn flat color="black" label="비밀번호 찾기" />
+        </div>
+      </div>
+      <div class="col-4">
+        <q-btn id="loginbtn" label="로그인" />
+        <br />
+        <q-btn id="joinbtn" flat color="primary" label="회원가입" />
+      </div>
+      <div class="col">
+        <div class="column items-center">
+          <img
+            style="width : 77px; height : 55px;"
+            src="~src/assets/ssafy_logo.png"
+          />
+          <br />
+          <img
+            style="width : 103px; height : 25px;"
+            src="~src/assets/im_enabling_people_logo.png"
+          />
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
+  </div>
 </template>
 <script>
-import Footer from "components/footer.vue"
+import Footer from "components/footer.vue";
 
 export default {
-  components: { Footer },
-}
+  components: { Footer }
+};
 </script>
 <style>
-.body {
-  background-color: #f1ecec;
-}
 #loginbtn {
   width: 190px;
   height: 35px;
