168b11d27b9fba13eef0327bf8dd946c64ad4d90
juheegg <eespacio95@gmail.com>
2021-02-11 18:40:11 +0900

    feat / SOL-178 / 회원탈퇴 페이지

--- front/src/pages/mypage/Withdrawal.vue
+++ front/src/pages/mypage/Withdrawal.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <h2>Withdrawal</h2>
+  <div class="q-pa-md">
+    <div class="row justify-center" style="margin: 20px;">
+      <div class="col-3"></div>
+      <div class="col-6" style="max-width: 420px">
+        <q-input
+          outlined
+          type="password"
+          v-model="password"
+          label="비밀번호 확인"
+          style="margin-top: 20px"
+        />
+
+        <div class="row justify-center" style="margin: 40px 0 0">
+          <q-btn
+            outline
+            size="14px"
+            label="탈퇴하기"
+            style="padding: 6px 10px; margin: 0 10px"
+          />
+        </div>
+      </div>
+      <div class="col-3"></div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: ""
+    };
+  },
+  methods: {}
+};
+</script>
