9f297be820aea452e453f1f54088b499f715f772
Park Sangwoo <tkddnsos@gmail.com>
2021-02-13 03:20:00 +0900

    refactor / SOL-215 / user 메인 버튼 componet 개발

---
+++ front/src/components/user/UserButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-btn id="btn" :label="label" @click="$emit('click')" />
+</template>
+<script>
+export default {
+  name: "UserButton",
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+<style lang="sass" scoped>
+#btn
+  width: 190px
+  height: 35px
+  font-size: 12px
+  color: #FFFFFF
+  align-content: center
+  background-color: #0094ff
+</style>
