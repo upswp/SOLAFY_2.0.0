169b99a3c31fdced9e875a59c267158dc0f28700
LEEAYOUNG <souk0711@gmail.com>
2021-02-12 23:53:03 +0900

    feat / SOL-170 / 메인페이지 틀 개발

--- front/src/pages/admin/Main.vue
+++ front/src/pages/admin/Main.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    관리자메인페이지
+  <div class="body">
+    <div class="text">
+      <p style="font-size:26px">관리자님 환영합니다</p>
+      <p style="font-size:12px">최근 홈페이지 데이터를 보고 드리겠습니다</p>
+    </div>
   </div>
 </template>
 <script>
 export default {};
 </script>
+<style lang="sass" scoped>
+.body
+    width:100%
+.text
+    text-align: center
+    margin-top: 20px
+    margin-bottom: 20px
+</style>
